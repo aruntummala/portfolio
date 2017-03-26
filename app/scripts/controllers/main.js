8fe3d8fdf2afbe62e53ab391ade7b69315de0f20
Arun Tummala <aruntummala@arun.corp.apple.com>
2017-03-25 23:13:45 -0700

    Paint balls showing up. Pushing to new paintBall branch.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,51 +6,41 @@
  * # MainCtrl
  * Controller of the artuApp
  */
-angular.module('artuApp').controller('MainCtrl', function($scope) {
+angular.module('artuApp').controller('MainCtrl', function ($scope) {
     var appName = $('.appName');
     console.log($scope);
-    var splitText = new SplitText($('.appName'), {
-        type: 'chars'
-    });
-    TweenMax.staggerFrom(splitText.chars, 0.5, {
-        opacity: 0.5,
-        rotation: -180,
-        y: -100,
-        ease: Back.easeOut
-    }, 0.02);
-    $('.screen_container').onepage_scroll({
-        sectionContainer: 'div.screen',
-        easing: "ease",
-        animationTime: 1000,
-        pagination: true,
-        updateURL: false,
-        loop: true,
-        keyboard: true,
-        responsiveFallback: false,
-        direction: "vertical",
-        beforeMove: function(index) {
-            if(index == 1) {
-                var tl = new TimelineMax();
-                tl.to(".myName", 3, {
-                    scrambleText: {
-                        text: "Arun Tummala",
-                        chars: "lowerCase",
-                        revealDelay: 2,
-                        tweenLength: false,
-                        ease: Back.easeOut
-                    }
-                });
-            }
-            $('.logos').css('visibility','hidden');
-        },
-        afterMove: function(index) {
-            if(index===2) {
-                $('.logos').css('visibility','visible');
-                TweenMax.staggerFrom([$('.logos')[0], $('.logos')[1], $('.logos')[2], $('.logos')[3], $('.logos')[4], $('.logos')[5], $('.logos')[6]], 3, {
-                    scale: 0.2,
-                    opacity: 0
-                }, 0.25);
-            }
-        }
-    });
+    var paintBallContainer = $(".paint-ball-container");
+    var counter = 0;
+    setInterval(function () {
+        counter++;
+        paintBallContainer.append('<div class="paint-ball-' + counter + ' '+randomFrom(['blue', 'cyan', 'green', 'red', 'yellow'])+'"></div>');
+        inittlMax('paint-ball-' + counter + '');
+    }, 1000);
+    var tlMax = new TimelineMax();
+    var tl = new TimelineLite();
+
+    function inittlMax(id) {
+        //tl.add(TweenLite.to($('.'+id), 1.5,{top: getRandomTop(), left: getRandomLeft()}));
+        tl.to($('.' + id), 1, {
+            top: getRandomTop(),
+            left: getRandomLeft(),
+            scale: 0.6,
+            opacity: 1
+        });
+        // tl.from($('.' + id), 1, {
+            
+        // });
+    };
+
+    function getRandomLeft() {
+        return Math.floor(Math.random() * (document.body.scrollWidth - 100 + 1)) + 100 + 'px';
+    };
+
+    function getRandomTop() {
+        return Math.floor(Math.random() * (document.body.scrollHeight - 100 + 1)) + 100 + 'px';
+    }
+
+    function randomFrom(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
 });
